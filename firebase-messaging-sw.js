da35ee93281d15fcdf779da9bb4f36032bfe73ac
GaroHills <124554185+GaroHills@users.noreply.github.com>
2026-01-23 00:32:36 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdbykOwpd71-dnGmgc-tgMQxNponPip2k",
+  authDomain: "gh-filemaker.firebaseapp.com",
+  databaseURL: "https://gh-filemaker-default-rtdb.firebaseio.com",
+  projectId: "gh-filemaker",
+  storageBucket: "gh-filemaker.firebasestorage.app",
+  messagingSenderId: "369203199821",
+  appId: "1:369203199821:web:9d2a497150a5130f31c104",
+  measurementId: "G-14T0WPR8FE"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://raw.githubusercontent.com/GaroHills/File-maker/refs/heads/main/file_0000000087307230923a773d76222c7b.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
