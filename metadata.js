e0e2f4c005ceb284aa3d1927207b8788b2a28d90
mrbtools <124554185+mrbtool@users.noreply.github.com>
2026-01-29 23:04:55 +0530

    Add 'earn_money' category with tags and color

--- metadata.js
+++ metadata.js
@@ -66,5 +66,10 @@ var toolMetadata = {
         tags: ["Super fast", "easy", "good"],
         color: "#00a86b"
     },
+        "earn_money": {
+        category: "Free Money",
+        tags: ["fast earn", "easy", "Unlimited"],
+        color: "#00a86b"
+    },
 
 };
